35f0f72a1462f0c78da34127d21cea5e0a511084
Ryan Guidice <rjgnospam@gmail.com>
2021-01-04 16:00:50 -0700

    Added in end-of-game menu to restart, change difficulty or end game.

--- ddr.py
+++ ddr.py
@@ -64,46 +64,82 @@ right_arrow = [
 
 directions = ['left', 'right', 'up', 'down']
 
-if args.difficulty == "easy":
-    delay = 3
-elif args.difficulty == "medium":
-    delay = 2
-else:
-    delay = 1
-
-score = 0
-
-for i in range(20):
-    sense.clear()
-    arrow = choice(directions)
+while True:
+    end_game = False
     
-    if arrow == 'left':
-        sense.set_pixels(left_arrow)
-    elif arrow == 'right':
-        sense.set_pixels(right_arrow)
-    elif arrow == 'up':
-        sense.set_pixels(up_arrow)
-    elif arrow == 'down':
-        sense.set_pixels(down_arrow)
+    #TROUBLESHOOTING:
+    print(args.difficulty)
+    
+    if args.difficulty == "easy":
+        delay = 3
+    elif args.difficulty == "medium":
+        delay = 2
+    else:
+        delay = 1
+
+    score = 0
+    
+    for i in range(10):
+        sense.clear()
+        arrow = choice(directions)
         
-    time.sleep(delay)
-    
-    user_event = 'none'
-
-    
-    for event in sense.stick.get_events():
-        if user_event != 'none':
-            break
-        if event.direction == 'left' or 'right' or 'down' or 'up':
-            user_event = event.direction
-        
-    if arrow == user_event:
-        score = score + 1
-    
-    print(score)
-    
-    
-print("Final Score: " + str(score))
-sense.show_message("Final Score: ", scroll_speed = 0.05)
-sense.show_message(str(score), scroll_speed = 0.2)
+        if arrow == 'left':
+            sense.set_pixels(left_arrow)
+        elif arrow == 'right':
+            sense.set_pixels(right_arrow)
+        elif arrow == 'up':
+            sense.set_pixels(up_arrow)
+        elif arrow == 'down':
+            sense.set_pixels(down_arrow)
             
+        time.sleep(delay)
+        
+        user_event = 'none'
+
+        
+        for event in sense.stick.get_events():
+            if user_event != 'none':
+                break
+            if event.action == 'pressed':
+                if event.direction in ['left', 'right', 'down', 'up']:
+                    user_event = event.direction
+            
+        if arrow == user_event:
+            score = score + 1
+        
+        print(score)
+        
+        
+    print("Final Score: " + str(score))
+    sense.show_message("Final Score: ", scroll_speed = 0.05)
+    sense.show_message(str(score), scroll_speed = 0.2)
+    
+    diff_menu = False
+    restart = False
+    
+    while True:
+        if restart == True or end_game == True:
+            break
+        for event in sense.stick.get_events():
+            if event.action == 'pressed':
+                if event.direction in ['left', 'right', 'down', 'up']:
+                    if diff_menu == False:
+                        if event.direction == 'up':
+                            restart = True
+                        if event.direction == 'left':
+                            end_game = True
+                        if event.direction == 'down':
+                            diff_menu = True
+                    else:
+                        if event.direction == 'up':
+                            args.difficulty = 'easy'
+                        elif event.direction == 'right':
+                            args.difficulty = 'medium'
+                        elif event.direction == 'left':
+                            args.difficulty = 'hard'
+                        restart = True
+                
+
+    if end_game == True:
+        break
+
